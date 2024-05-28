3551ccd51dfe5d299812fe322f349092cba1a742
Kenneth Tan <featurete@gmail.com>
2024-05-29 00:55:47 +0700

    cobain

--- lib/common_widget/round_button.dart
+++ lib/common_widget/round_button.dart
@@ -1,64 +1,82 @@
 import 'package:fitness_tracker/common/colo_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-enum RoundButtonType {bgGradient, textGradient}
+enum RoundButtonType {bgGradient, bgSGradient , textGradient}
 
 class RoundButton extends StatelessWidget {
   final String title;
   final RoundButtonType type;
   final VoidCallback onPressed;
+  final double fontSize;
+  final FontWeight fontWeight;
+
   const RoundButton(
     {super.key, 
     required this.title, 
     this.type = RoundButtonType.bgGradient, 
+    this.fontSize = 16,
+    this.fontWeight = FontWeight.w700,
     required this.onPressed});
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: TColor.primaryG,
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight),
+          colors: type == RoundButtonType.bgSGradient ? TColor.secondaryG : TColor.primaryG,
+          
+        ),
         borderRadius: BorderRadius.circular(25),
-        boxShadow: type ==  RoundButtonType.bgGradient 
-        ? const [
-          BoxShadow(
-            color: Colors.black26, 
-            blurRadius: 0.5, 
-            offset: Offset(0, 0.5))
-            ] 
-          : null),
+        boxShadow: type == RoundButtonType.bgGradient || type == RoundButtonType.bgSGradient
+            ? const [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 0.5,
+                  offset: Offset(0, 0.5),
+                ),
+              ]
+            : null,
+      ),
       child: MaterialButton(
         onPressed: onPressed,
-          height: 50,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-          textColor: TColor.primaryColor1,
-          minWidth: double.maxFinite,
-          elevation: type ==  RoundButtonType.bgGradient ? 0 : 1,
-          color: type ==  RoundButtonType.bgGradient ? Colors.transparent : TColor.white,
-          child: type ==  RoundButtonType.bgGradient ?  Text(title,
-              style: TextStyle(
-                color: TColor.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w700))
-            : ShaderMask(
-            blendMode: BlendMode.srcIn,
-            shaderCallback: (bounds) {
-              return LinearGradient(
-                colors: TColor.primaryG, 
-                begin: Alignment.centerLeft, 
-                end: Alignment.centerRight)
-                .createShader(Rect.fromLTRB(
-                  0, 0, bounds.width, bounds.height));
-            },
-            child: Text(title,
-              style: TextStyle(
-                color: TColor.primaryColor1,
-                fontSize: 18,
-                fontWeight: FontWeight.w700)),
+        height: 50,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
         ),
+        textColor: TColor.primaryColor1,
+        minWidth: double.maxFinite,
+        elevation: type == RoundButtonType.bgGradient || type == RoundButtonType.bgSGradient ? 0 : 1,
+        color: type == RoundButtonType.bgGradient || type == RoundButtonType.bgSGradient
+            ? Colors.transparent
+            : TColor.white,
+        child: type == RoundButtonType.bgGradient || type == RoundButtonType.bgSGradient
+            ? Text(
+                title,
+                style: TextStyle(
+                  color: TColor.white,
+                  fontSize: 16,
+                  fontWeight: fontWeight,
+                ),
+              )
+            : ShaderMask(
+                blendMode: BlendMode.srcIn,
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                    colors: TColor.primaryG,
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height));
+                },
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: TColor.primaryColor1,
+                    fontSize: 16,
+                    fontWeight: fontWeight,
+                  ),
+                ),
+              ),
       ),
     );
   }
